60c747ae6c4c0a1c86368dd2f53e3217f2032823
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2020-07-10 07:05:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,18 +1,3 @@
 class Song
-
-  def name=(name)
-    @name = name
-  end
-
-  def name
-    @name
-  end
-
-  def title=(title)
-    @title = title
-  end
-
-  def title
-    @title
-  end
+  attr_accessor :name
 end
